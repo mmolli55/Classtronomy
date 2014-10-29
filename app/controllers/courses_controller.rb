82aab3e8aef12bb30e13613fda1a1800e82466e5
mmolli55 <matthewjosephmolli@gmail.com>
2014-10-28 23:00:57 -0400

    styling changes to welcome page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,55 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update(params[:course].permit(:name, :paragraph1, :image, :paragraph2, :video, :paragraph3))
+      redirect_to @course
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+
+    redirect_to courses_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(
+      :name,
+      :paragraph1,
+      :image,
+      :paragraph2,
+      :video,
+      :paragraph3
+      )
+  end
+
 end
